3c72ce8bb22669025fddfd800317eed866353dde
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2025-07-24 00:00:51 +0800

    Update flash light stage with dark lightmap

--- Assets/Scripts/Stages/Flood/FindFlashLight.cs
+++ Assets/Scripts/Stages/Flood/FindFlashLight.cs
@@ -2,22 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CoroutineUtility;
+using System.Linq;
 
 public class FindFlashLight : Stage
 {
-    public GameObject pointLights;
-
     public GameObject flashlight;   // 手電筒
     public Light globalLight;   // 環境光源
+    public Light[] _roomLights;
+
+    [SerializeField] Texture2D[] _lowLightTextures;
+    [SerializeField] Texture2D[] _lowDirTextures;
 
     public override void OnBegin()
     {
         base.OnBegin();
 
-        pointLights.SetActive(false);
         DG.Tweening.DOTween.To(
             () => globalLight.intensity, x => globalLight.intensity = x, .015f, .5f
         );
+
+        foreach (var light in _roomLights)
+            light.enabled = false;
+
+        var lightmaps = LightmapSettings.lightmaps.ToArray();
+        for (int i = 0; i < lightmaps.Length; ++i)
+        {
+            lightmaps[i].lightmapColor = _lowLightTextures[0];
+            lightmaps[i].lightmapDir = _lowDirTextures[0];
+        }
+        LightmapSettings.lightmaps = lightmaps;
 
         JacDev.Audio.Flood a = (JacDev.Audio.Flood)GameHandler.Singleton.audioHandler;
         a.PlaySound(a.getRescueKit);
