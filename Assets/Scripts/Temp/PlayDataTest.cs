6b092a5f3ae459d7ef46c93856cbf8135a478025
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2025-09-02 15:21:56 +0800

    add debug multi play data

--- Assets/Scripts/Temp/PlayDataTest.cs
+++ Assets/Scripts/Temp/PlayDataTest.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class PlayDataTest : MonoBehaviour
 {
     public List<MissionSetting> Settings { get; private set; } = new();
+    [field: SerializeField]
+    public List<string> PlayerNames { get; private set; } = new();
+    [SerializeField]
+    bool _autoSend;
 
     [ContextMenu("Test Build Full Data")]
     void Test()
     {
-        foreach (var mission in Settings)
+        var origin = GameHandler.playerData;
+
+        foreach (var p in PlayerNames)
         {
-            GameHandler.playerData.SetMissionData(mission.name);
-            foreach (var stgSetting in mission.settings)
+            GameHandler.playerData = new(p);
+            foreach (var mission in Settings)
             {
+                GameHandler.playerData.SetMissionData(mission.missionName);
+                foreach (var stgSetting in mission.settings)
+                {
 
-                var stgData = new PlayerData.MissionData.StgData();
-                stgData.stgName = stgSetting.name;
-                stgData.score = stgSetting.score;
-                stgData.time = Random.Range(0, 1000f);
+                    var stgData = new PlayerData.MissionData.StgData();
+                    stgData.stgName = stgSetting.name;
+                    stgData.score = stgSetting.score;
+                    stgData.time = Random.Range(0, 1000f);
 
-                GameHandler.playerData.SetStageData(stgData);
+                    GameHandler.playerData.SetStageData(stgData);
+                }
+            }
+
+            if (_autoSend)
+            {
+                GameHandler.Singleton.SavePlayerData();
+            }
+            else
+            {
+                return;
             }
         }
+
+        GameHandler.playerData = origin;
     }
 }
